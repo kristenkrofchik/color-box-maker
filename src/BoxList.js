5ccc019e0be86cd92d04e2a79afc916308247c48
Kristen Krofchik <k.krofchik@gmail.com>
2021-06-19 23:46:24 -0500

    work on BoxList component

--- src/BoxList.js
+++ src/BoxList.js
@@ -6,6 +6,27 @@ import { v4 as uuid } from "uuid";
 function BoxList() {
     const [boxes, setBoxes] = useState([]);
 
-    const addBox = newBox => {
-        setBoxes(boxes => [...boxes, newBox]); 
+    const renderBoxes = () => {
+        return (
+          <ul>
+            {boxes.map((width, height, backgroundColor, id) => (
+              <Box width={width} height={height} backgroundColor={backgroundColor} id={id}/>
+            ))}
+          </ul>
+        );
       };
+
+    const addBox = box => {
+        let newBox = { ...box, id: uuid() };
+        setBoxes(boxes => [...boxes, newBox]);
+      };
+
+    return (
+        <div className="BoxList">
+          <NewBoxForm addBox={addBox} />
+          {renderBoxes()}
+        </div>
+      );
+};
+
+export default BoxList;
